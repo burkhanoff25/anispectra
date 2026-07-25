ab5bd30ba08bf44d5c3f6b091da562fd90ecd7b1
burkhanoff25 <burkhanoff25@users.noreply.github.com>
2026-07-26 03:14:13 +0500

    fix build uuid type error

--- src/app/watch-party/page.tsx
+++ src/app/watch-party/page.tsx
@@ -1,8 +1,7 @@
 import { redirect } from "next/navigation";
-import { v4 as uuidv4 } from "uuid";
 
 export default function WatchPartyIndex() {
   // Avtomatik ravishda tasodifiy xona yaratib, o'sha xonaga yo'naltirish
-  const newRoomId = uuidv4();
+  const newRoomId = crypto.randomUUID();
   redirect(`/watch-party/${newRoomId}`);
 }
