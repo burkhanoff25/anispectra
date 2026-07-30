791328d7941068376394a078655a4f30a7cb6b11
burkhanoff25 <burkhanoff25@users.noreply.github.com>
2026-07-30 17:35:17 +0500

    Add new Yandex verification for www

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,7 +81,7 @@ export const metadata: Metadata = {
   },
   verification: {
     google: 'hdYUKiHpQKzyvzF-sBnfzR_CEWnu6p9NfSJstHqZcCs',
-    yandex: '3e3bfcd1ce4cdf77',
+    yandex: '9ee519ae2d9fb53d',
   },
 };
 
